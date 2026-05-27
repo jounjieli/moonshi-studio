1dd9f76cb44f0c77469a5240fb19f7a77160e09f
jinjieLi <42171953+jounjieli@users.noreply.github.com>
2026-05-27 22:22:02 +0800

    Add Moonshi Studio site files

---
+++ assets/js/site-config.js
@@ -0,0 +1,7 @@
+window.MOONSHI_CONFIG = {
+  brandName: "Moonshi 沐煦",
+  instagramUrl: "https://www.instagram.com/moonshi_studio/?hl=zh-tw",
+  lineUrl: "https://line.me/R/ti/p/@300shhin",
+  addressArea: "台中，完成預約後提供詳細地址與到店說明。",
+  mapUrl: "",
+};
