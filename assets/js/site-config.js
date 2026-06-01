e6654afa6f5e591581180f673ddd889c3096d9d0
jinjieLi <42171953+jounjieli@users.noreply.github.com>
2026-06-01 23:39:34 +0800

    Add Google Maps navigation link

--- assets/js/site-config.js
+++ assets/js/site-config.js
@@ -3,5 +3,5 @@ window.MOONSHI_CONFIG = {
   instagramUrl: "https://www.instagram.com/moonshi_studio/?hl=zh-tw",
   lineUrl: "https://line.me/R/ti/p/@300shhin",
   addressArea: "台中，完成預約後提供詳細地址與到店說明。",
-  mapUrl: "",
+  mapUrl: "https://maps.app.goo.gl/b6Y2gaBuo3NjVb1F7",
 };
